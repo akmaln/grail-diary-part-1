b43f5a4c846d9ca8eac5fc3b97890edc3fb0bd80
Akmal Nurmatov <akmalnurmatov1@gmail.com>
2020-03-17 17:44:13 -0400

    completed the main.storyboard

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Akmal Nurmatov on 3/17/20.
+//  Copyright © 2020 Akmal Nurmatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
